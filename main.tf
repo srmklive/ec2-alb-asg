52860e99694dc56abb4d48060aa48641fcf0bd39
Raza Mehdi <srmk@outlook.com>
2023-11-25 21:07:39 +0500

    update scripts.

--- main.tf
+++ main.tf
@@ -1,40 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  backend "s3" {
-    bucket = "srmk-terraform-state"
-    encrypt = true
-    key = "terraform.infra.state"
-    region = "us-east-1"
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {}
-
-module "web" {
-  source = "./modules/web"
-
-  ami           = var.ami
-  instance_type = var.instance_type
-  instance_tag  = var.instance_tag
-  key_name      = var.key_name
-  subnet_id     = aws_subnet.tf-web.id
-  vpc_id        = aws_vpc.tf-vpc.id
-}
-
 module "autoscaling" {
   source = "./modules/alb"
-  depends_on = [module.web]
 
-  ami           = module.web.ec2_ami
-  security_group = module.web.sg_tf_web
+  ami           = var.ami
+  security_group = aws_security_group.sg-tf-web.id
   instance_type = var.instance_type
   instance_tag  = var.instance_tag
   key_name      = var.key_name
